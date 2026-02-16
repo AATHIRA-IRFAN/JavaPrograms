b162d83f1ae47a7724f65973e298ba69f8198176
AATHIRA-IRFAN <aathira733@gmail.com>
2026-02-16 20:07:48 +0530

    success

--- src/org/pgm/AnagramProgram.java
+++ src/org/pgm/AnagramProgram.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class AnagramProgram {
 
 	public static void main(String[] args) {
+		System.out.println("Pgm Started");
 		String s1 = "silent";
 		String s2 = "listen";
 		
